cf87a65993f4e9e3972782b19bd88f65c2a1059b
Asao <kamei@workspot.jp>
2013-02-24 17:33:02 +0900

    added comment.

--- src/WScore/Template/TemplateInterface.php
+++ src/WScore/Template/TemplateInterface.php
@@ -3,11 +3,61 @@ namespace WScore\Template;
 
 interface TemplateInterface
 {
+    /**
+     * sets a template file. 
+     * 
+     * @param string $name
+     * @return mixed
+     */
     public function setTemplate( $name );
+
+    /**
+     * sets parent template file.
+     * 
+     * @param $parentTemplate
+     * @return mixed
+     */
+    public function setParent( $parentTemplate );
+
+    /**
+     * sets a value. 
+     * 
+     * @param string $name
+     * @param mixed  $value
+     * @return mixed
+     */
     public function set( $name, $value );
+
+    /**
+     * sets a value only if it is not set yet. 
+     * 
+     * @param string $name
+     * @param mixed  $value
+     * @return mixed
+     */
     public function setDefault( $name, $value );
+
+    /**
+     * sets array of data.
+     * 
+     * @param array $data
+     * @return mixed
+     */
     public function assign( $data );
-    public function parent( $parentTemplate );
+
+    /**
+     * gets a value, or the $default if not set. 
+     * 
+     * @param string       $name
+     * @param null|mixed   $default
+     * @return mixed
+     */
     public function get( $name, $default=null );
+
+    /**
+     * renders a template.
+     * 
+     * @return mixed
+     */
     public function render();
 }
