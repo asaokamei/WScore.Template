f6a5f5f68c70312390d8ce8e872ab590907ddbd7
Asao <kamei@workspot.jp>
2013-02-24 17:06:29 +0900

    simplified.

--- src/WScore/Template/Renderer.php
+++ src/WScore/Template/Renderer.php
@@ -11,20 +11,20 @@ class Renderer
     // +----------------------------------------------------------------------+
     /**
      * @param string $template
-     * @param array  $parameters
+     * @param array  $_parameters
      * @throws \RuntimeException
      * @return mixed
      */
-    public function render( $template, $parameters = array() )
+    public function render( $template, $_parameters = array() )
     {
-        // attach the global variables
-        if( $this->self ) {
-            $content = ob_get_clean();
-        }
-        else {
-            $content = $this->evaluate( $template, $parameters );
-        }
-        return $content;
+        $_parameters[ '__template__' ] = $template;
+
+        /** @var $__template__ string */
+        extract( $_parameters, EXTR_SKIP );
+        ob_start();
+        require $__template__;
+
+        return ob_get_clean();
     }
 
     /**
@@ -36,31 +36,12 @@ class Renderer
     }
 
     /**
-     * Evaluates a template.
-     *
-     * @param string  $template   The template to render
-     * @param array   $parameters An array of parameters to pass to the template
-     * @return string|bool The evaluated template, or false if the engine is unable to render the template
-     */
-    protected function evaluate( $template, array $parameters = array())
-    {
-        $parameters[ '__template__' ] = $template;
-
-        /** @var $__template__ string */
-        extract( $parameters, EXTR_SKIP );
-        ob_start();
-        require $__template__;
-
-        return ob_get_clean();
-    }
-
-    /**
      * render output if rendering self output.
      */
     public function __destruct()
     {
         if( $this->self ) {
-            echo $this->render( $this->templateFile );
+            echo ob_end_clean();
         }
     }
 }
