8ef45d2ddd8696675eb9c2a144bfbd8872b68f4f
Asao <kamei@workspot.jp>
2013-03-01 06:51:16 +0900

    added a test suites for template.

---
+++ tests/Template/All_Template_Tests.php
@@ -0,0 +1,17 @@
+<?php
+namespace WScore\tests\Html;
+
+ini_set( 'display_errors', 1 );
+error_reporting( E_ALL );
+
+class All_Template_Tests
+{
+    public static function suite()
+    {
+        $suite = new \PHPUnit_Framework_TestSuite( 'all tests for WScore\'s Template' );
+        $suite->addTestFile( __DIR__ . '/PhpTemplate_Test.php' );
+        $suite->addTestFile( __DIR__ . '/PhpTemplate_Root_Test.php' );
+
+        return $suite;
+    }
+}
